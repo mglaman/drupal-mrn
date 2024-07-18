7baf7a71219317a32d429eb10af36cd69c511998
Joe Parsons <471936+joeparsons@users.noreply.github.com>
2024-07-18 09:21:10 -0700

    #164: Extract author & committer usernames from emails if fetching usernames from API fails. (#210)
    * #164: Use extracted usernames from author and committer emails if fetching usernames from API fails.
    
    * Fix whitespace.
    
    * Move regex variable outside of foreach loop.
    
    * make sure tests cover 403 from the API
    
    ---------
    
    Co-authored-by: Matt Glaman <nmd.matt@gmail.com>

--- api/src/Changelog.php
+++ api/src/Changelog.php
@@ -35,6 +35,7 @@ final class Changelog
         if (count($commits) === 0) {
             throw new \RuntimeException('No commits for the changelog to process.');
         }
+        $emailUsernameRegex = '/(?<=[0-9]-)([a-zA-Z0-9-_\.]{2,255})(?=@users\.noreply\.drupalcode\.org)/';
         $contributors = [];
         foreach ($commits as $commit) {
             $commitContributors = CommitParser::extractUsernames($commit->title);
@@ -43,13 +44,21 @@ final class Changelog
                 if (count($author) > 0) {
                     $commitContributors[] = $author[0]->username;
                 }
-            } catch (RequestException) {}
+            } catch (RequestException) {
+                if (preg_match($emailUsernameRegex, $commit->author_email, $authorMatches)) {
+                    $commitContributors[] = $authorMatches[0];
+                }
+            }
             try {
                 $committer = $gitlab->users($commit->committer_name);
                 if (count($committer) > 0) {
                     $commitContributors[] = $committer[0]->username;
                 }
-            } catch (RequestException) {}
+            } catch (RequestException) {
+                if (preg_match($emailUsernameRegex, $commit->committer_email, $committerMatches)) {
+                    $commitContributors[] = $committerMatches[0];
+                }
+            }
             $contributors[] = $commitContributors;
 
             $nid = CommitParser::getNid($commit->title);
